033aea91a130788d6a7bc5f4d4612fac87ecd998
shigerysasaki <shigerysasaki12@hotmail.com>
2023-09-30 12:33:28 -0300

    Adicionar arquivos de projeto.

---
+++ microdundamento desenvolvimento backend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace microdundamento_desenvolvimento_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
